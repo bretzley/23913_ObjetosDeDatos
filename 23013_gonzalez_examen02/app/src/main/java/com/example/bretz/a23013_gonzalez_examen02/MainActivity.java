678486b5b0940bbf1d8a90284e6a264edb100eed
bretzley <bretzley@gotribit.com>
2017-10-27 16:25:59 -0700

    +Examen 2 project base upload

---
+++ 23013_gonzalez_examen02/app/src/main/java/com/example/bretz/a23013_gonzalez_examen02/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bretz.a23013_gonzalez_examen02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
